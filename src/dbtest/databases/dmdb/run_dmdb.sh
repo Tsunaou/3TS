2278e32c471965f86b9221cba974e1e388ee1748
tsunaouyang <tsunaouyang@tencent.com>
2023-01-28 02:46:33 +0800

    Update support for dmdb

---
+++ src/dbtest/databases/dmdb/run_dmdb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+DBTEST_DIR="/root/github-projects/3TS-Coo/src/dbtest"
+PROGRAM="3ts_dbtest_v2"
+BUILD_DIR=$DBTEST_DIR"/build/"
+CASE_DIR=$DBTEST_DIR"/t/test_case_v2"
+DB_DIR=$DBTEST_DIR"/databases/dmdb"      # to config
+LIST_DIR=$DB_DIR"/do_test_list_dmdb.txt" # to config
+OUTPUT_DIR=$DB_DIR
+RUNNER=$BUILD_DIR$PROGRAM
+
+$RUNNER \
+    -isolation=serializable \
+    -db_type="dm" \
+    -user="SYSDBA" -passwd="SYSDBA001" \
+    -conn_pool_size=4 \
+    -timeout=1 \
+    -case_dir=$CASE_DIR \
+    -out_dir=$OUTPUT_DIR \
+    -do_test_list=$LIST_DIR
