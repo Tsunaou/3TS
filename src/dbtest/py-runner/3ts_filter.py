a77becc8703ae6fa661abc9738c890f284bfc168
tsunaouyang <tsunaouyang@tencent.com>
2023-01-31 20:37:07 +0800

    Update Opt and Filter for patterns

--- src/dbtest/py-runner/3ts_filter.py
+++ src/dbtest/py-runner/3ts_filter.py
@@ -1,6 +1,12 @@
 import re
 # from re import Pattern
 
+do_test_list_path = '../do_test_list_base.txt'
+opt1_list_path = '../opt1.txt'
+opt2_list_path = '../opt2.txt'
+opt3_list_path = '../opt3.txt'
+
+
 # invalid conflicts: II IW WI ID DI DD (also need to include 'C', e.g., ICI, in total 12 = 6*2)
 invalid_conflicts = [
     r'IC*I', r'IC*W', r'WC*I', r'IC*D', r'DC*I', r'DC*D',
@@ -9,25 +15,23 @@ invalid_conflicts = [
 # invalid pattern: WW-WW WW-WR WW-RW WR-WW WR-WR WR-RW RW-WW RW-RW
 # (also need to include 'C', e.g., WCW0-WW0 WW-WCW WCW1-WCW1, in total 64 = 8*4(with and without c)*2 (object 0 or 1))
 invalid_patterns = [
-    r'WC*W0-WC*W0',
-    r'WC*W0-WC*R0',
-    r'WC*W0-RC*W0',
-    r'WC*R0-WC*W0',
-    r'WC*R0-WC*R0',
-    r'WC*W0-RC*W0',
-    r'RC*W0-WC*W0',
-    r'RC*W0-RC*W0',
-    r'WC*W1-WC*W1',
-    r'WC*W1-WC*R1',
-    r'WC*W1-RC*W1',
-    r'WC*R1-WC*W1',
-    r'WC*R1-WC*R1',
-    r'WC*W1-RC*W1',
-    r'RC*W1-WC*W1',
-    r'RC*W1-RC*W1',
+    r'[W|I]C*[W|I]0-[W|I]C*[W|I]0',
+    r'[W|I]C*[W|I]0-[W|I]C*R0',
+    r'[W|I]C*[W|I]0-RC*[W|I]0',
+    r'[W|I]C*R0-[W|I]C*[W|I]0',
+    r'[W|I]C*R0-[W|I]C*R0',
+    r'[W|I]C*[W|I]0-RC*[W|I]0',
+    r'RC*[W|I]0-[W|I]C*[W|I]0',
+    r'RC*[W|I]0-RC*[W|I]0',
+    r'[W|I]C*[W|I]1-[W|I]C*[W|I]1',
+    r'[W|I]C*[W|I]1-[W|I]C*R1',
+    r'[W|I]C*[W|I]1-RC*[W|I]1',
+    r'[W|I]C*R1-[W|I]C*[W|I]1',
+    r'[W|I]C*R1-[W|I]C*R1',
+    r'[W|I]C*[W|I]1-RC*[W|I]1',
+    r'RC*[W|I]1-[W|I]C*[W|I]1',
+    r'RC*[W|I]1-RC*[W|I]1',
 ]
-
-do_test_list_path = '../do_test_list_base.txt'
 
 
 def match_pattern(p, testcase: str):
@@ -42,23 +46,9 @@ def match_pattern(p, testcase: str):
     return True
 
 
-if __name__ == '__main__':
-    with open(do_test_list_path, 'r') as f:
-        testcases = list(map(str.strip, f.readlines()))
-
-    # 统计testcase个数数据
-    # OPT1 only remove "P"
-    p1 = re.compile('P')
-    opt1_cases = list(filter(lambda x: p1.findall(x) == [], testcases))
-    # OPT2 remove "P" and remove invalid pattern
-    p2 = re.compile('|'.join(invalid_conflicts))
-    opt2_cases = list(filter(lambda x: match_pattern(p2, x), opt1_cases))
-    # OPT3 remove "P" ， invalid pattern，invalid conflicts
-    p3 = re.compile('|'.join(invalid_patterns))
-    opt3_cases = list(filter(lambda x: match_pattern(p3, x), opt2_cases))
-
+def report_results(opt0_cases, opt1_cases, opt2_cases, opt3_cases):
     print('-' * 100)
-    print('Original number:', len(testcases))
+    print('Opt0 number:', len(opt0_cases))
     print('-' * 100)
     print('Opt1 number:', len(opt1_cases))
     print('-' * 100)
@@ -69,3 +59,44 @@ if __name__ == '__main__':
     print('Opt3 number:', len(opt3_cases))
     for case in opt3_cases:
         print(case)
+
+
+def write_do_test_list(testcases, path):
+    with open(path, 'w') as f:
+        for case in testcases:
+            f.write(case)
+            f.write('\n')
+
+
+if __name__ == '__main__':
+    with open(do_test_list_path, 'r') as f:
+        opt0_cases = list(map(str.strip, f.readlines()))
+
+    # 统计testcase个数数据
+    # OPT1 only remove "P"
+    p1 = re.compile('P')
+    opt1_cases = list(filter(lambda x: p1.findall(x) == [], opt0_cases))
+    # OPT2 remove "P" and remove invalid pattern
+    p2 = re.compile('|'.join(invalid_patterns))
+    opt2_cases = list(filter(lambda x: match_pattern(p2, x), opt1_cases))
+    # OPT3 remove "P" ， invalid pattern，invalid conflicts
+    p3 = re.compile('|'.join(invalid_conflicts))
+    opt3_cases = list(filter(lambda x: match_pattern(p3, x), opt2_cases))
+
+    # report_results(opt0_cases, opt1_cases, opt2_cases, opt3_cases)
+
+    # write_do_test_list(opt1_cases, opt1_list_path)
+    # write_do_test_list(opt2_cases, opt2_list_path)
+    # write_do_test_list(opt3_cases, opt3_list_path)
+
+    databases = ['dmdb', 'hgdb',  'oceanbase',
+                 'oracle',  'polardb',  'postgres']
+
+    for db in databases:
+        path = '../report/{}/cycles.log'.format(db)
+        with open(path, 'r') as f:
+            cycles = set(map(str.strip, f.readlines()))
+        print('Found {} cycles while testing {}'.format(len(cycles), db))
+        print('-- {} cycles are remained after opt1'.format(len(cycles.intersection(set(opt1_cases)))))
+        print('-- {} cycles are remained after opt2'.format(len(cycles.intersection(set(opt2_cases)))))
+        print('-- {} cycles are remained after opt3'.format(len(cycles.intersection(set(opt3_cases)))))
